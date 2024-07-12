c5fd855d5407eab262a4268b8f7ec48d52766fcb
SIVAKUMAR CHIMMILI <sivafrom99@gmail.com>
2024-07-12 11:57:32 +0530

    find the missing nos in the array ranging from 1 to n

---
+++ missingNosInArray.js
@@ -0,0 +1,19 @@
+//find the missing no in the array ranging from 1 to n :
+function missingNoInArray(n,arr) {
+    let set = new Set(arr);
+    let missingNos = [];
+    for (let i=1;i<=n;i++) {
+        if(!set.has(i)) {
+            missingNos.push(i);
+        }
+    }
+    return missingNos
+}
+
+// let n=11;
+// let arr= [1,2,3,5,7,8];
+
+let n=5;
+let arr = [2,3,5]
+
+console.log(missingNoInArray(n,arr));
